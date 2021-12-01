55ff653eaadbd5ca1c018153159f14423b37813d
Camilo Riobamba <camilo.riobamba@gmail.com>
2021-12-01 14:47:22 -0500

    feat: add illustration and breakpoints object

--- src/Illustration.js
+++ src/Illustration.js
@@ -5,11 +5,20 @@ import boxDesktop from './images/illustration-box-desktop.svg';
 import illustrationWoman from './images/illustration-woman-online-mobile.svg';
 import illustrationWomanDesktop from './images/illustration-woman-online-desktop.svg';
 
+//imports
+import breakpoints from "./breakpoints";
+
 export default class Illustration extends React.Component{
     render(){
         return (
             <div>
-                <img src={illustrationWoman} alt=""/>
+                <picture>
+                    <source src={illustrationWomanDesktop} media={`(min-width: ${breakpoints.md}px)`}/>
+                    <img src={illustrationWoman} alt="Illustration of faq"/>
+                </picture>
+                <div>
+                    <img src={boxDesktop} alt=""/>
+                </div>
             </div>
         );
     }
