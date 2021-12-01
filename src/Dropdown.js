ff865ee74b288cfab65365f906c584886b6b02e1
Camilo Riobamba <camilo.riobamba@gmail.com>
2021-12-01 10:02:26 -0500

    feat: add toogle dropdown

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -4,6 +4,21 @@ import React from "react";
 import arrowDown from './images/icon-arrow-down.svg';
 
 export default class Dropdown extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            active: false
+        }
+
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+
+    toggleDropdown(){
+        this.setState((prevState) => ({
+            active: !prevState.active
+        }))
+    }
+
     render(){
         return (
             <div>
@@ -11,11 +26,16 @@ export default class Dropdown extends React.Component{
                     <span>
                         <h3>{this.props.title}</h3>
                     </span>
-                    <span>
-                        <img src={arrowDown} alt="arrow down"/>
+                    <span onClick={this.toggleDropdown}>
+                        {
+                            this.state.active ?
+
+                            <img src={arrowDown} alt="arrow down"/> :
+                            <img src={arrowDown} alt="arrow down" className="rotate-180"/>
+                        }
                     </span>
                 </div>
-                <div>
+                <div className={ this.state.active ? "d-block" : "d-none" }>
                     <p>
                         {this.props.content}
                     </p>
