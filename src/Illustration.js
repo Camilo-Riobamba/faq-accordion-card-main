41d82c25008be149e4f5c7a71f2e01915de4bfc0
Camilo Riobamba <camilo.riobamba@gmail.com>
2021-12-01 21:45:15 -0500

    feat: remove multiple illustration

--- src/Illustration.js
+++ src/Illustration.js
@@ -1,24 +1,13 @@
 import React from "react";
 
 //media
-import boxDesktop from './images/illustration-box-desktop.svg';
 import illustrationWoman from './images/illustration-woman-online-mobile.svg';
-import illustrationWomanDesktop from './images/illustration-woman-online-desktop.svg';
-
-//imports
-import breakpoints from "./breakpoints";
 
 export default class Illustration extends React.Component{
     render(){
         return (
-            <div>
-                <picture>
-                    <source src={illustrationWomanDesktop} media={`(min-width: ${breakpoints.md}px)`}/>
-                    <img src={illustrationWoman} alt="Illustration of faq"/>
-                </picture>
-                <div>
-                    <img src={boxDesktop} alt=""/>
-                </div>
+            <div className="text-center">
+                <img src={illustrationWoman} alt="illustration" className="w-100"/>
             </div>
         );
     }
